390aa6c8a78c663c4be73da345a17c9aa958cc3f
Sindre Moldeklev <sndrem@live.no>
2015-02-25 23:13:32 +0100

    Revert 5405b9a..fb56c67
    This rolls back to commit 5405b9a98cc534ae6521afe2e37ad88d9523bff5.

---
+++ src/no/uib/smo015/info233/oblig2/Events/ListEvent.java
@@ -0,0 +1,34 @@
+package no.uib.smo015.info233.oblig2.Events;
+
+import java.text.SimpleDateFormat;
+
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import no.uib.smo015.info233.oblig2.Activity.Activity;
+import no.uib.smo015.info233.oblig2.GUI.Gui;
+
+public class ListEvent implements ListSelectionListener {
+	
+	private Gui gui;
+	
+	public ListEvent(Gui gui){
+		this.gui = gui;
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		Activity activity = gui.getActivityList().getSelectedValue();
+		SimpleDateFormat sdf = new SimpleDateFormat("EEEE, d MMM, yyyy");
+		if(activity != null){
+			gui.getWeekDayLabel().setText("Dag: " + sdf.format(activity.getStartTime().getTime()).substring(0,1).toUpperCase() + sdf.format(activity.getStartTime().getTime()).substring(1));
+			gui.getDescLabel().setText("Beskrivelse: " + activity.getDescription());
+			gui.getTypeLabel().setText("Type: " + activity.getType());
+			gui.getTimeStartLabel().setText("Starter: " + activity.getStartTimeString());
+			gui.getTimeEndLabel().setText("Slutter: " + activity.getEndTimeString());
+			gui.getRoomLabel().setText("Rom: " + activity.getRoom());
+		} 
+		
+	}
+
+}
---
+++ src/no/uib/smo015/info233/oblig2/Events/WindowEvent.java
@@ -0,0 +1,79 @@
+package no.uib.smo015.info233.oblig2.Events;
+
+import java.awt.event.WindowListener;
+
+import no.uib.smo015.info233.oblig2.GUI.Gui;
+import no.uib.smo015.info233.oblig2.UIBRoomApp.UibRoomApp;
+import no.uib.smo015.info233.oblig2.Util.InternetUtil;
+
+/**
+ * Class representing an event that happens when the user interacts with the window
+ * @author Sindre
+ * @version 0.0.1
+ *
+ */
+public class WindowEvent implements WindowListener {
+
+		private Gui gui;
+
+		/**
+		 * Constructor for the WindowEvent class
+		 * @param gui
+		 */
+		public WindowEvent(Gui gui){
+			this.gui = gui;
+		}
+		
+		@Override
+		public void windowOpened(java.awt.event.WindowEvent e) {
+			
+		}
+
+		/**
+		 * Method invoked when the user closes the application by the X in the corner of the frame.
+		 * The method first checks if the user has internet connectivity and if yes, then it saves whatever the user is 
+		 * looking at. Otherwise it won't save because we might save inconsistent data or no data at all.
+		 */
+		@Override
+		public void windowClosing(java.awt.event.WindowEvent e) {
+			if(InternetUtil.hasConnectivity()){
+				UibRoomApp.saveFile("testGui2");
+			} else {
+				System.out.println("No connection to internet. \n"
+						+ "I wont overwrite your stuff. No worries bro..");
+			}
+		}
+
+		@Override
+		public void windowClosed(java.awt.event.WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowIconified(java.awt.event.WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowDeiconified(java.awt.event.WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowActivated(java.awt.event.WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowDeactivated(java.awt.event.WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+
+
